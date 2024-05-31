895bc2135445261e91fe374b18f270ec40af2c1a
tharkijailer69 <angelroshni949@gmail.com>
2024-06-01 00:34:38 +0530

    Update raid.py

--- OXYBOT/modules/raid.py
+++ OXYBOT/modules/raid.py
@@ -36,11 +36,11 @@ async def raid(e):
 
         try:
             if uid in OXYGEN:
-                await e.reply("REPO OWNER HAI YE.")
+                await e.reply("TOXIC KA REPO  HAI YE.")
             elif uid == OWNER_ID:
-                await e.reply("BETA BAAP PE RAID NHI KRTE HAI...")
+                await e.reply("BAAP PE KAREGA RAID TERI MAA CHOD DUNGA...")
             elif uid in SUDO_USERS:
-                await e.reply("ISPE RAID MAT MARO.. !!...")
+                await e.reply("TOXIC KA DOST HAI BSDK RAID MT MAAR.. !!...")
             else:
                 first_name = entity.first_name
                 counter = int(xraid[1])
@@ -101,11 +101,11 @@ async def rraid(e):
         try:
             user_id = entity.id
             if user_id in OXYGEN:
-                await e.reply("REPO OWNER HAI YE.")
+                await e.reply("TOXIC KA REPO  HAI YE.")
             elif user_id == OWNER_ID:
-                await e.reply("BETA BAAP PE RAID NHI KRTE HAI....")
+                await e.reply("BAAP PE KAREGA RAID TERI MAA CHOD DUNGA....")
             elif user_id in SUDO_USERS:
-                await e.reply("= ISPE RAID MAT MARO.. !!...")
+                await e.reply("= TOXIC KA DOST HAI BSDK RAID MT MAAR.. !!...")
             else:
                 global REPLY_RAID
                 check = f"{user_id}_{e.chat_id}"
